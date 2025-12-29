7cd876aff2bdc5dfefdb262e30cfdc18920078a0
wargacemplangrt007 <wargacemplang007@gmail.com>
2025-12-29 20:23:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 const sheetURL =
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vRxp9RzXU7MNibquVZ6wYj6s6zzyySpmF6IG5OGNg5UTp_dkMnAHF3JIDGLbxnW2V8J9Oew1s3xu3-7/pub?output=csv";
+"https://docs.google.com/spreadsheets/d/e/2PACX-1vRxp9RzXU7MNibquVZ6wYj6s6zzyySpmF6IG5OGNg5UTp_dkMnAHF3JIDGLbxnW2V8J9Oew1s3xu3-7/pub?output=csv";
 
 fetch(sheetURL)
-  .then(res => res.text())
+  .then(response => response.text())
   .then(data => {
     const rows = data.split("\n").slice(1);
     let html = "";
 
     rows.reverse().forEach(row => {
-      const col = row.split(",");
-      if (col.length >= 4) {
+      const cols = row.split(",");
+
+      if (cols.length >= 4) {
         html += `
           <div class="pengumuman-item">
-            <h3>${col[2]}</h3>
-            <small>${col[1]}</small>
-            <p>${col[3]}</p>
+            <h3>${cols[1]}</h3>
+            <small>${cols[2]}</small>
+            <p>${cols[3]}</p>
+            <hr>
           </div>
         `;
       }
     });
 
-    document.getElementById("pengumuman-list").innerHTML =
+    document.getElementById("pengumuman-data").innerHTML =
       html || "<p>Belum ada pengumuman.</p>";
+  })
+  .catch(() => {
+    document.getElementById("pengumuman-data").innerHTML =
+      "<p>Gagal memuat pengumuman.</p>";
   });
